1640bbe3a2919d93b94551d00c467986fc803347
Marlo Broch <jbroc24@student.sdu.dk>
2026-03-20 01:29:21 +0100

    merge
    :O

--- LibraryApp/ViewModels/LoginViewModel.cs
+++ LibraryApp/ViewModels/LoginViewModel.cs
@@ -1,10 +1,50 @@
-﻿//login logic - username, password, error message etc?
-using System;
+﻿using System;
+using Avalonia.Controls;
 using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using System.Text.Json;
+using System.IO;
 
 namespace LibraryApp.ViewModels;
 
+// what I am trying to do is that I activate this script if the login button is pressed, than it compares the model and sees if in the json file
+// there is a user with that name and password. After that it checks with member type is this. This will link it t MemberMainView or LibarianMainView
 public partial class LoginViewModel : ViewModelBase
 {
+
+    [ObservableProperty]
+    private string _username = "";
+
+    [ObservableProperty]
+    private string _password = "";
+    
+
+
+    [RelayCommand]
+    private void Login()
+    {
+  
+        string fileName = "login.json";
+        string jsonString = File.ReadAllText(fileName);
+
+        UserContainer? container = JsonSerializer.Deserialize<UserContainer>(jsonString);
+        
+        if(container.username == _username && container.password == _password)
+        {
+            //check if libarian or member
+            if(container.role == "member")
+            {
+                //point to member site
+                
+            }
+            else
+            {
+                // point to lib site
+            }
+            
+        }
+        
+    }   
+    
 
 }
